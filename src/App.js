f3661f3f51430c035aaa2df17a7e7dd55ac0139a
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-02-19 22:58:39 +0530

    CREATE: styles added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import React from "react";
 import data from "./Data";
 import Card from "./Card";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+  const clearAll = () => {
+    setPeople([]);
+    console.log("cleared");
+  };
+
   return (
     <main>
       <section>
-        {data.map((person) => {
+        <h2>{people.length} birthdays today</h2>
+        {people.map((person) => {
           return (
-            <Card image={person.image} name={person.name} age={person.age} />
+            <Card
+              image={person.image}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+            />
           );
         })}
+        <button className="clear" onClick={clearAll}>
+          Clear All
+        </button>
       </section>
     </main>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,16 @@
-function Card({ image, name, age }) {
+import { FaTimes } from "react-icons/fa";
+
+function Card({ image, name, age, id }) {
   return (
-    <div className="card-container">
+    <div className="card-container" key={id}>
       <img src={image} alt={name} />
       <div className="card-info">
         <h4>{name}</h4>
         <p>{age}</p>
       </div>
+      <button>
+        <FaTimes />
+      </button>
     </div>
   );
 }
